bc8aea80514c89ee2038671a0462efe2f820d919
Gutemberg <gutemberg.cz@gmail.com>
2024-11-25 16:16:24 -0300

    Initial commit

---
+++ src/main/java/com/gtb/geneport/GeneportApplication.java
@@ -0,0 +1,13 @@
+package com.gtb.geneport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtb/geneport/GeneportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtb.geneport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
